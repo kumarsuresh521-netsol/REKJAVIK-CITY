053609b82901339d321fbc40e5da2568f0ea1b55
kumarsuresh521-netsol <kumarsuresh521.netsol@gmail.com>
2015-12-16 14:57:13 +0530

    REKJAVIK  CITY
    REKJAVIK  CITY

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,112 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geofence/www/TransitionType.js",
+        "id": "cordova-plugin-geofence.TransitionType",
+        "clobbers": [
+            "TransitionType"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geofence/www/geofence.js",
+        "id": "cordova-plugin-geofence.geofence",
+        "clobbers": [
+            "geofence"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "file": "plugins/uk.co.workingedge.phonegap.plugin.LaunchNavigator/www/android/launchnavigator.js",
+        "id": "uk.co.workingedge.phonegap.plugin.LaunchNavigator.LaunchNavigator",
+        "clobbers": [
+            "launchnavigator"
+        ]
+    },
+    {
+        "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.js",
+        "id": "cordova.plugins.diagnostic.Diagnostic",
+        "clobbers": [
+            "cordova.plugins.diagnostic"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/es6-promise-plugin/www/promise.js",
+        "id": "es6-promise-plugin.Promise",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-android-support-v4": "21.0.1",
+    "cordova-plugin-console": "1.0.1",
+    "cordova-plugin-device": "1.0.1",
+    "cordova-plugin-geofence": "0.4.2",
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-splashscreen": "2.1.0",
+    "cordova-plugin-statusbar": "1.0.1",
+    "cordova-plugin-whitelist": "1.0.0",
+    "ionic-plugin-keyboard": "1.0.7",
+    "uk.co.workingedge.phonegap.plugin.LaunchNavigator": "2.9.2",
+    "cordova.plugins.diagnostic": "1.1.0",
+    "cordova-plugin-inappbrowser": "1.1.1",
+    "es6-promise-plugin": "3.0.2",
+    "cordova-plugin-googleplayservices": "19.0.3"
+}
+// BOTTOM OF METADATA
+});
